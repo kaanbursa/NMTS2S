725c35eb40227439998b50e1fd0459042ffa6617
kaan bursa <kaanbursa9@gmail.com>
2020-03-31 16:14:42 +0300

    model finished

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,125 @@
 import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+import json
 
-class Vocabulary():
+
+
+class NMTDataset(Dataset):
+    def __init__(self, text_df, vectorizer):
+        """
+        Args:
+            surname_df (pandas.DataFrame): the dataset
+            vectorizer (SurnameVectorizer): vectorizer instatiated from dataset
+        """
+        self.text_df = text_df
+        self._vectorizer = vectorizer
+
+        self.train_df = self.text_df[self.text_df.split=='train']
+        self.train_size = len(self.train_df)
+
+        self.val_df = self.text_df[self.text_df.split=='val']
+        self.validation_size = len(self.val_df)
+
+        self.test_df = self.text_df[self.text_df.split=='test']
+        self.test_size = len(self.test_df)
+
+        self._lookup_dict = {'train': (self.train_df, self.train_size),
+                             'val': (self.val_df, self.validation_size),
+                             'test': (self.test_df, self.test_size)}
+
+        self.set_split('train')
+
+    @classmethod
+    def load_dataset_and_make_vectorizer(cls, dataset_csv):
+        """Load dataset and make a new vectorizer from scratch
+
+        Args:
+            surname_csv (str): location of the dataset
+        Returns:
+            an instance of SurnameDataset
+        """
+        text_df = pd.read_csv(dataset_csv)
+        train_subset = text_df[text_df.split=='train']
+        return cls(text_df, NMTVectorizer.from_dataframe(train_subset))
+
+    @classmethod
+    def load_dataset_and_load_vectorizer(cls, dataset_csv, vectorizer_filepath):
+        """Load dataset and the corresponding vectorizer.
+        Used in the case in the vectorizer has been cached for re-use
+
+        Args:
+            surname_csv (str): location of the dataset
+            vectorizer_filepath (str): location of the saved vectorizer
+        Returns:
+            an instance of SurnameDataset
+        """
+        text_df = pd.read_csv(dataset_csv)
+        vectorizer = cls.load_vectorizer_only(vectorizer_filepath)
+        return cls(text_df, vectorizer)
+
+
+    @staticmethod
+    def load_vectorizer_only(vectorizer_filepath):
+        """a static method for loading the vectorizer from file
+
+        Args:
+            vectorizer_filepath (str): the location of the serialized vectorizer
+        Returns:
+            an instance of SurnameVectorizer
+        """
+        with open(vectorizer_filepath) as fp:
+            return NMTVectorizer.from_serializable(json.load(fp))
+
+    def save_vectorizer(self, vectorizer_filepath):
+        """saves the vectorizer to disk using json
+
+        Args:
+            vectorizer_filepath (str): the location to save the vectorizer
+        """
+        with open(vectorizer_filepath, "w") as fp:
+            json.dump(self._vectorizer.to_serializable(), fp)
+
+    def get_vectorizer(self):
+        """ returns the vectorizer """
+        return self._vectorizer
+
+    def set_split(self, split="train"):
+        self._target_split = split
+        self._target_df, self._target_size = self._lookup_dict[split]
+
+    def __len__(self):
+        return self._target_size
+
+    def __getitem__(self, index):
+        """the primary entry point method for PyTorch datasets
+
+        Args:
+            index (int): the index to the data point
+        Returns:
+            a dictionary holding the data point: (x_data, y_target, class_index)
+        """
+        row = self._target_df.iloc[index]
+
+        vector_dict = self._vectorizer.vectorize(row.source_language, row.target_language)
+
+        return {"x_source": vector_dict["source_vector"],
+                "x_target": vector_dict["target_x_vector"],
+                "y_target": vector_dict["target_y_vector"],
+                "x_source_length": vector_dict["source_length"]}
+
+    def get_num_batches(self, batch_size):
+        """Given a batch size, return the number of batches in the dataset
+
+        Args:
+            batch_size (int)
+        Returns:
+            number of batches in the dataset
+        """
+        return len(self) // batch_size
+
+class Vocabulary(object):
     def __init__(self, token_to_index=None):
 
         if token_to_index is None:
@@ -11,7 +130,7 @@ class Vocabulary():
 
     def to_serializable(self):
         """ Returns a dictionary that can be serialized"""
-        return {'token_to_idx' : self._token_to_index}
+        return {'token_to_index' : self._token_to_index}
 
     @classmethod
     def from_serializable(cls, contents):
@@ -24,7 +143,7 @@ class Vocabulary():
         :param token: the item thats gonna be added to Vocabulary
         :return: the integer corresponding
         """
-        if token not in self._token_to_index:
+        if token in self._token_to_index:
             index = self._token_to_index[token]
         else:
             index = len(self._token_to_index)
@@ -59,9 +178,9 @@ class Vocabulary():
         return len(self._token_to_index)
 
 
-def SequenceVocabulary(Vocabulary):
+class SequenceVocabulary(Vocabulary):
     def __init__(self, token_to_index = None, unk_token="<UNK>", mask_token="<MASK>", begin_seq_token="<BEGIN>", end_seq_token="<END>"):
-        super(Vocabulary, self).__init__(token_to_index)
+        super(SequenceVocabulary, self).__init__(token_to_index)
 
         self._mask_token = mask_token
         self._unk_token = unk_token
@@ -69,20 +188,23 @@ def SequenceVocabulary(Vocabulary):
         self._end_seq_token = end_seq_token
 
         self.mask_index = self.add_token(self._mask_token)
-        self.unk_token = self.add_token(self._unk_token)
-        self.begin_seq_token = self.add_token(self._begin_seq_token)
-        self.end_seq_token = self.add_token(self._end_seq_token)
+        self.unk_index = self.add_token(self._unk_token)
+        self.begin_seq_index = self.add_token(self._begin_seq_token)
+        self.end_seq_index = self.add_token(self._end_seq_token)
 
     def to_serializable(self):
         contents = super(SequenceVocabulary, self).to_serializable()
-        contents.update({'unk_token':self._unk_token, 'mask_token':self._mask_token, 'begin_seq_token': self._begin_seq_token, 'end_seq_token':self._end_seq_token})
+        contents.update({'unk_token':self._unk_token,
+                        'mask_token':self._mask_token,
+                        'begin_seq_token': self._begin_seq_token,
+                        'end_seq_token':self._end_seq_token})
         return contents
 
     def lookup_token(self, token):
         if self._unk_index >= 0:
-            return self._token_to_idx.get(token, self._unk_index)
+            return self._token_to_index.get(token, self._unk_index)
         else:
-            return self._token_to_idx[token]
+            return self._token_to_index[token]
 
 
 
